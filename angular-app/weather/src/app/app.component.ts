a7aaa4c1088fe37fe69329a51d2bc626db76951a
Jeevitha Kumaresan <jeevithak@virtusa.com>
2018-06-15 17:26:13 +0530

    added weather first commit

---
+++ angular-app/weather/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { WeatherService } from './weather.service';
+import { ISummary } from './models/summary';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  city: string;
+  constructor(public weatherService: WeatherService) {
+  }
+
+  onButtonClick(cityName: string) {
+    console.log(cityName);
+    this.weatherService.fetchWeatherInfo(cityName);
+  }
+
+}
